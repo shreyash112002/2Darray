b4dfe7bc05131d1a34bbf20a998b35677adc8f38
Shreyash Y Patil <shreyashypatil1122002@gmail.com>
2021-10-05 17:09:37 +0530

    Char array

---
+++ src/Char.java
@@ -0,0 +1,15 @@
+
+    import java.io.IOException;
+
+    class Char
+    {
+        public static void main(String[] args) throws IOException
+        {
+            char ch;
+            System.out.println("Enter a character ");
+            ch=(char) System.in.read();
+
+            System.out.println(ch);
+        }
+    }
+
---
+++ src/Sum.java
@@ -0,0 +1,16 @@
+
+//W.A.P to accept 2 integers & display their sum using CLA
+
+class Sum1
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        a=Integer.parseInt(args[0]);
+        b=Integer.parseInt(args[1]);
+        c=a+b;
+
+        System.out.println("Sum is "+c);
+    }
+}
+
